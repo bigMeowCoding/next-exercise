a9ea4b78d07c53570e046bcd1da12e6a85d82b98
zhouyijun <zhou495248579@hotmail.com>
2023-10-13 18:23:59 +0800

    chore:init

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-// const withSass = require("@zeit/next-sass");
-// const withCss = require("@zeit/next-css");
-//
-// if (typeof require !== "undefined") {
-//   require.extensions[".css"] = (file) => {};
-// }
-//
-// module.exports = withCss(withSass({}));
-const withImages = require("next-images");
-module.exports = withImages();
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
